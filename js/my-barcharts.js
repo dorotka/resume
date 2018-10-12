d3b023bff8d4e1df98e5d8e3d8efacdd02e36d74
Dorotka <dorota_oleszczuk@yahoo.com>
2018-10-12 19:45:04 +0200

    cleanup of skills on mobile

--- js/my-barcharts.js
+++ js/my-barcharts.js
@@ -6,68 +6,68 @@ if(!isOldIE()){
 }
 
 function drawMainSkillsGraphs() {
-          var techChart = new CanvasJS.Chart("chartMainTech", {
-            theme: "theme1",
-            axisY:{
-              gridColor: "#fff",
-              lineThickness: 1,
-             },
-             axisX:{
-              lineThickness: 1,
-             },
-             toolTip: {
-              content: "<div class=\"tipName\"> {label}</div><div>Level: {y} out of 100</div>",
-              borderColor: "#eeeeee", 
-              animationEnabled: true
-             },
-             data: [              
-              {
-                  type: "bar",
-                  color: "#7b6888", 
-                  dataPoints: [
-                  { label: "AngulaJs", y: 95 },
-                  { label: "Javascript", y: 95 },
-                  { label: "Java", y: 85 },
-                  { label: "Python", y: 80 },
-                  { label: "SQL", y: 70 },
-                  { label: "GIT", y: 70 }
-                  ]
-              }
-             ]
-          });
+    var techChart = new CanvasJS.Chart("chartMainTech", {
+      theme: "theme1",
+      axisY:{
+        gridColor: "#fff",
+        lineThickness: 1,
+       },
+       axisX:{
+        lineThickness: 1,
+       },
+       toolTip: {
+        content: "<div class=\"tipName\"> {label}</div><div>Level: {y} out of 100</div>",
+        borderColor: "#eeeeee", 
+        animationEnabled: true
+       },
+       data: [              
+        {
+            type: "bar",
+            color: "#7b6888", 
+            dataPoints: [
+            { label: "AngulaJs", y: 95 },
+            { label: "Javascript", y: 95 },
+            { label: "Java", y: 85 },
+            { label: "Python", y: 80 },
+            { label: "SQL", y: 70 },
+            { label: "GIT", y: 70 }
+            ]
+        }
+       ]
+    });
 
 /*  var techSkillsData = {};*/
 
-          var artChart = new CanvasJS.Chart("chartMainArt", {
-            theme: "theme1",
-            axisY:{
-              gridColor: "#fff",
-              lineThickness: 1,
-             },
-             axisX:{
-              lineThickness: 1,
-             },
-             toolTip: {
-              content: "<div class=\"tipName\"> {label}</div><div>Level: {y} out of 100</div>",
-              borderColor: "#eeeeee", 
-              animationEnabled: true
-             },
-             data: [              
-              {
-                  type: "bar",
-                  color: "#7b6888", 
-                  dataPoints: [
-                  { label: "Scrum", y: 80 },
-                  { label: "Architecture and Design", y: 80 },
-                  { label: "Data Visualization", y: 90 },
-                  { label: "Data Mining", y: 80 }
-                  ]
-              }
-             ]
-          });
+    var artChart = new CanvasJS.Chart("chartMainArt", {
+      theme: "theme1",
+      axisY:{
+        gridColor: "#fff",
+        lineThickness: 1,
+       },
+       axisX:{
+        lineThickness: 1,
+       },
+       toolTip: {
+        content: "<div class=\"tipName\"> {label}</div><div>Level: {y} out of 100</div>",
+        borderColor: "#eeeeee", 
+        animationEnabled: true
+       },
+       data: [              
+        {
+            type: "bar",
+            color: "#7b6888", 
+            dataPoints: [
+            { label: "Scrum", y: 80 },
+            { label: "Architecture and Design", y: 80 },
+            { label: "Data Visualization", y: 90 },
+            { label: "Data Mining", y: 80 }
+            ]
+        }
+       ]
+    });
 
-          techChart.render();
-          artChart.render();
+    techChart.render();
+    artChart.render();
 }
 
         /********************** helper methods **********************/
